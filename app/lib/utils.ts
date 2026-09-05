b24bc70ba44aee5ac8ff40b0fca233fb7179fbf6
ezekiel-charo <ezekiel3499@gmail.com>
2026-09-05 05:09:25 +0300

    Add a debouce utility function

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -9,3 +9,18 @@ export function formatNumber(value?: number | null): string {
   if (!value && value !== 0) return "";
   return new Intl.NumberFormat().format(Math.abs(value));
 }
+
+export function debounce<T extends (...args: never[]) => void>(
+  func: T,
+  wait: number,
+): (...args: Parameters<T>) => void {
+  let timeoutId: ReturnType<typeof setTimeout>;
+
+  return (...args: Parameters<T>) => {
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => {
+      func(...args);
+    }, wait);
+  };
+}
