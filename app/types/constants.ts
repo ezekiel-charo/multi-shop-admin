c651b6e97bca981ea218b1e588cfeb54c0350855
ezekiel-charo <ezekiel3499@gmail.com>
2026-09-06 01:28:32 +0300

    Populate dummy data in db.json

--- app/types/constants.ts
+++ app/types/constants.ts
@@ -1,4 +1,8 @@
-export const BASE_URL = "https://mock-api-production-ff48.up.railway.app";
+const IS_DEV = import.meta.env.DEV;
+
+export const BASE_URL = IS_DEV
+  ? "http://localhost:3300"
+  : "https://mock-api-production-ff48.up.railway.app";
 export const TOKEN_KEY = "auth_token";
 
 export const DEFAULT_PAGE_SIZE = 10;
