000b24be6e273857cb93361e58afc6816c6d26f5
ezekiel-charo <ezekiel3499@gmail.com>
2026-09-05 20:18:20 +0300

    Fix token issue

--- app/lib/axios.ts
+++ app/lib/axios.ts
@@ -6,8 +6,19 @@ const api = axios.create({
   baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
-    Authorization: `Bearer ${getToken()}`,
   },
+});
+
+api.interceptors.request.use((config) => {
+  const token = getToken();
+
+  if (token) {
+    config.headers.set("Authorization", `Bearer ${token}`);
+  } else {
+    config.headers.delete("Authorization");
+  }
+
+  return config;
 });
 
 api.interceptors.response.use(
